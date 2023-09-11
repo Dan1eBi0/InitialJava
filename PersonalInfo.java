b093c3b88aef8b6a8cd20df6c61e7c23903e1d35
Daniela <40619472+Dan1eBi0@users.noreply.github.com>
2023-09-11 19:37:32 -0300

    Create PersonalInfo.java

---
+++ PersonalInfo.java
@@ -0,0 +1,20 @@
+public class PersonalInfo{
+
+    public static void main(String[] args){
+        System.out.println();
+        System.out.println();
+        System.out.println("Minha aplicação Java\n");
+
+        //tipo nomedaVariável;
+        int idade = 37;
+        double peso = 66; //var peso = 66.0
+        char sexo = 'F';
+        boolean eAlta = false; //var eAlta = false; var em Java cria uma variável local
+
+        System.out.println("A idade é: " + idade + "; O peso é: " + peso);
+        System.out.println("O sexo é: " + sexo);
+        System.out.println("É alta? " + eAlta);
+
+
+    }
+}
